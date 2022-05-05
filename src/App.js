543548bbc5fe92e24b79283af82e6508f3929610
Salvador Alvarez <sao.95@hotmail.com>
2022-05-04 21:37:53 -0700

    mostrar los personajes en el html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
-
+import Characters from "./components/Characters";
 
 function App() {
-  return (
-   <>
-       <Navbar brand="Rick and Morty App" />
+  const [characters, setCharacters] = useState([]);
 
-   
-   </>
+  let urlInitial = "https://rickandmortyapi.com/api/character";
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then((datos) => datos.json())
+      .then((info) => setCharacters(info.results))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchCharacters(urlInitial);
+  }, []);
+
+  return (
+    <>
+      <Navbar brand="Rick and Morty App" />
+      <div className="container">
+    <Characters  characters={characters}></Characters>
+      </div>
+    </>
   );
 }
 
